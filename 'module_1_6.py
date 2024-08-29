f952b435aae91bf4d4c3d32883ddaf986dc645ec
Smetanka2002 <needfrig@gmail.com>
2024-08-29 20:39:49 +0300

    Add files via upload

---
+++ 'module_1_6.py
@@ -0,0 +1,18 @@
+# 2023/09/25 00:00|Практическое задание по теме: "Словари и множества"
+my_dict = {"Ilya" : 89321228902, "Dilya" : 89314313124}
+print(my_dict)
+print(my_dict["Ilya"])
+my_dict["Misha"] = 891232453
+print(my_dict["Misha"])
+my_dict.update({"Svetlana" : 8932131232, "Oleg" : 89231212345})
+print(my_dict)
+my_dict.pop("Misha")
+print(my_dict)
+
+
+my_set = {1, 2, 1, 2, "Ilya", "Ilya"}
+print(my_set)
+my_set.add(5)
+my_set.add(True)
+my_set.remove(1)
+print(my_set)
