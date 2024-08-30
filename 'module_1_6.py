cb893d39089365d2490a809a447f767016d6a12d
Smetanka2002 <needfrig@gmail.com>
2024-08-30 18:35:00 +0300

    Add files via upload

--- 'module_1_6.py
+++ 'module_1_6.py
@@ -2,11 +2,13 @@
 my_dict = {"Ilya" : 89321228902, "Dilya" : 89314313124}
 print(my_dict)
 print(my_dict["Ilya"])
+print(my_dict.get("Denis", "Такого ключа нет!"))
 my_dict["Misha"] = 891232453
 print(my_dict["Misha"])
 my_dict.update({"Svetlana" : 8932131232, "Oleg" : 89231212345})
 print(my_dict)
-my_dict.pop("Misha")
+a = my_dict.pop("Misha")
+print(a)
 print(my_dict)
 
 
